c01b192cbd93d4fe271e1f249d240ced09292b67
Zhang Yingzhe <1300012722@pku.edu.cn>
2016-04-14 21:17:18 +0800

    Add JZlipUtil
    Signed-off-by: Zhang Yingzhe <1300012722@pku.edu.cn>

---
+++ src/main/java/sewm/bdbox/util/JZlipUtil.java
@@ -0,0 +1,28 @@
+package sewm.bdbox.util;
+
+import java.io.ByteArrayOutputStream;
+import java.util.zip.Inflater;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class JZlipUtil {
+  private static Logger logger = LogManager.getLogger(JZlipUtil.class);
+
+  public static byte[] decompress(byte[] data) {
+    Inflater inflater = new Inflater();
+    inflater.setInput(data);
+    byte[] buffer = new byte[1024];
+    try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+      while (!inflater.finished()) {
+        int count = inflater.inflate(buffer);
+        outputStream.write(buffer, 0, count);
+      }
+      byte[] output = outputStream.toByteArray();
+      return output;
+    } catch (Exception e) {
+      logger.error(ExceptionUtil.getStacktraceString(e));
+      return null;
+    }
+  }
+}
