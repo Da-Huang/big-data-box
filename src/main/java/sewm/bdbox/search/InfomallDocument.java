50fb1958a2dae3a47c94a9fe8b83cf69af9314e7
Zhang Yingzhe <1300012722@pku.edu.cn>
2016-04-18 16:23:06 +0800

    change InfomallDocument

--- src/main/java/sewm/bdbox/search/InfomallDocument.java
+++ src/main/java/sewm/bdbox/search/InfomallDocument.java
@@ -7,11 +7,19 @@ public class InfomallDocument {
   String url;
   Date date;
   String data;
-  public InfomallDocument(String version, String url, Date date, String data) {
+  Long position;
+  String charset;
+  String title;
+  String content;
+  public InfomallDocument(String version, String url, Date date, String data,Long position,String charset,String title,String content) {
     this.version = version;
     this.url = url;
     this.date = date;
     this.data = data;
+    this.position = position;
+    this.charset = charset;
+    this.title = title;
+    this.content = content;
   }
 
 }
