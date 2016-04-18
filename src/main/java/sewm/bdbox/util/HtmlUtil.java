1fcbb57149eea663c745845f90642d2853902b3d
Zhang Yingzhe <1300012722@pku.edu.cn>
2016-04-18 16:25:06 +0800

    Newest HtmlUtil

--- src/main/java/sewm/bdbox/util/HtmlUtil.java
+++ src/main/java/sewm/bdbox/util/HtmlUtil.java
@@ -2,15 +2,50 @@ package sewm.bdbox.util;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import sewm.bdbox.search.InfomallDocument;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.mozilla.universalchardet.UniversalDetector;
 
 public class HtmlUtil {
   private static Logger logger = LogManager.getLogger(HtmlUtil.class);
 
-  public static String pCharset(String data) {
-    if (data.length() > 200)
-      data = data.substring(0, 200);
+  // text, charset
+//  public static Entry<String, String> decode(byte[] data, List<String> encodings) {
+//    for (String encoding : encodings) {
+//      try {
+//        return new AbstractMap.SimpleEntry<String, String>(Charset
+//            .forName(encoding).newDecoder()
+//            .onMalformedInput(CodingErrorAction.REPORT)
+//            .onUnmappableCharacter(CodingErrorAction.REPLACE)
+//            .decode(ByteBuffer.wrap(data)).toString(), encoding);
+//      } catch (Exception e) {
+//        logger.warn("Try to use another encoding.");
+//      }
+//    }
+//    return null;
+//  }
+
+  public static String pCharset(UniversalDetector detector, byte[] data) {
+    // String str = new String(data);
+    // String encoding = pCharset(str);
+    // if (encoding != null) {
+    // return encoding;
+    // }
+    // return "gb18030";
+    String encoding;
+    detector.reset();
+    detector.handleData(data, 0, data.length);
+    detector.dataEnd();
+    encoding = detector.getDetectedCharset();
+
+    return encoding != null ? encoding : "gb18030";
+  }
+
+  private static String pCharset(String data) {
+    // (5)
+    if (data.length() > 400)
+      data = data.substring(0, 400);
+    // logger.info(data);
     data = data.toLowerCase();
     while (true) {
       int i = data.indexOf("<meta");
@@ -34,14 +69,34 @@ public class HtmlUtil {
       }
 
       String item[] = s.split(" |;|'|\"", 2);
-      //logger.info(item[0]);
+      // logger.info(item[0]);
       if (item[0].isEmpty()) {
-        return "utf8";
+        return "gb18030";
       } else if (item[0].subSequence(0, 2).equals("gb"))
         return "gb18030";
-      else
-        return "utf8";
+      else {
+        logger.info(item[0]);
+        return item[0];
+      }
     }
-    return "utf8";
+    return null;
+  }
+
+  public static String parseTitle(String data) {
+    // data = data.toLowerCase();
+    // int i = data.indexOf("<title>");
+    // int j = data.indexOf("</title>", i);
+    // if (j == -1 || i == -1)
+    // return null;
+    // String s = data.substring(i + 7, j);
+    Document doc = Jsoup.parse(data);
+    String str = doc.title();
+    return str;
+  }
+
+  public static String parseContent(String data) {
+    org.jsoup.nodes.Document doc = Jsoup.parse(data);
+    String str = doc.text();
+    return str;
   }
 }
