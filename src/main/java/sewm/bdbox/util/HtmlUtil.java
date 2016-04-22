b38fb9bc3738ec363f135da32b1a0d0480a72ba4
Zhang Yingzhe <1300012722@pku.edu.cn>
2016-04-22 19:30:32 +0800

    html add host

--- src/main/java/sewm/bdbox/util/HtmlUtil.java
+++ src/main/java/sewm/bdbox/util/HtmlUtil.java
@@ -1,5 +1,8 @@
 package sewm.bdbox.util;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jsoup.Jsoup;
@@ -9,43 +12,18 @@ import org.mozilla.universalchardet.UniversalDetector;
 public class HtmlUtil {
   private static Logger logger = LogManager.getLogger(HtmlUtil.class);
 
-  // text, charset
-//  public static Entry<String, String> decode(byte[] data, List<String> encodings) {
-//    for (String encoding : encodings) {
-//      try {
-//        return new AbstractMap.SimpleEntry<String, String>(Charset
-//            .forName(encoding).newDecoder()
-//            .onMalformedInput(CodingErrorAction.REPORT)
-//            .onUnmappableCharacter(CodingErrorAction.REPLACE)
-//            .decode(ByteBuffer.wrap(data)).toString(), encoding);
-//      } catch (Exception e) {
-//        logger.warn("Try to use another encoding.");
-//      }
-//    }
-//    return null;
-//  }
-
   public static String pCharset(UniversalDetector detector, byte[] data) {
-    // String str = new String(data);
-    // String encoding = pCharset(str);
-    // if (encoding != null) {
-    // return encoding;
-    // }
-    // return "gb18030";
     String encoding;
     detector.reset();
     detector.handleData(data, 0, data.length);
     detector.dataEnd();
     encoding = detector.getDetectedCharset();
-
     return encoding != null ? encoding : "gb18030";
   }
 
   private static String pCharset(String data) {
-    // (5)
     if (data.length() > 400)
       data = data.substring(0, 400);
-    // logger.info(data);
     data = data.toLowerCase();
     while (true) {
       int i = data.indexOf("<meta");
@@ -75,7 +53,7 @@ public class HtmlUtil {
       } else if (item[0].subSequence(0, 2).equals("gb"))
         return "gb18030";
       else {
-        logger.info(item[0]);
+
         return item[0];
       }
     }
@@ -83,20 +61,29 @@ public class HtmlUtil {
   }
 
   public static String parseTitle(String data) {
-    // data = data.toLowerCase();
-    // int i = data.indexOf("<title>");
-    // int j = data.indexOf("</title>", i);
-    // if (j == -1 || i == -1)
-    // return null;
-    // String s = data.substring(i + 7, j);
     Document doc = Jsoup.parse(data);
     String str = doc.title();
     return str;
   }
 
   public static String parseContent(String data) {
+    int i = data.indexOf("html");
+    data = data.substring(i + 5);
     org.jsoup.nodes.Document doc = Jsoup.parse(data);
     String str = doc.text();
     return str;
+  }
+
+  public static String parseHost(String url) {
+    URL aUrl;
+    try {
+      aUrl = new URL(url);
+      String host = aUrl.getHost();
+      return host;
+    } catch (MalformedURLException e) {
+      // TODO Auto-generated catch block
+      logger.error(ExceptionUtil.getStacktraceString(e));
+      return null;
+    }
   }
 }
