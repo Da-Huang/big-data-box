98583774b30e8ae7bc0a32d75b0829efddc8d873
Da Huang <dhuang.cn@gmail.com>
2016-04-24 13:50:11 +0800

    Bash index.
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

---
+++ index.sh
@@ -0,0 +1,6 @@
+#/usr/bin/bash
+
+data=/Volumes/HPT8_56T/data/U200201/Web_Raw.U200201.0001
+index=/Volumes/HPT8_56T/index
+java -Xms6g -Xmx6g -cp target/big-data-box.jar:target/lib/* sewm.bdbox.search.InfomallIndexer --data=$data --index=$index --create
+sendemail -f sewm_pku@yeah.net -t dhuang.cn@gmail.com -s smtp.yeah.net -u "Index for $data has been built!" -m "Please check $index." -xu sewm_pku -xp sewm1220
