df105688406f26348e7de6f82ca1c27a36a52bcc
Da Huang <dhuang.cn@gmail.com>
2016-05-15 14:18:13 +0800

    Built graph page.
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

---
+++ src/main/webapp/static/script/graph.js
@@ -0,0 +1,14 @@
+function FetchNode() {
+  var form = $('.graph_form');
+  var query = form.find('[name=url]').val();
+  console.log('Query', query);
+  _CleanResults();
+  $.getJSON(form.attr('action'), query).done(function(top_docs) {
+    console.log('Result', top_docs);
+  }).fail(function(msg) {
+    console.log(msg.statusText);
+  });
+}
+
+function _CleanResults() {
+}
